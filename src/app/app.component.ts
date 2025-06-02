88dc6d3b6c6229a8cb4931a50572b5f517f644b6
cami-1310 <al347948@edu.uaa.mx>
2025-06-02 17:45:23 -0600

    conexion a la base de datos y prueba altas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { HttpClientModule } from '@angular/common/http';
+import { AltasPruebaComponent } from './altas-prueba/altas-prueba.component';
 
 
 @Component({
   selector: 'app-root',
   standalone:true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent,HttpClientModule],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent,HttpClientModule, AltasPruebaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
