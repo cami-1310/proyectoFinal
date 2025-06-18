b9986d1cc1557af6145d2dab619169da0a1cf17f
cami-1310 <al347948@edu.uaa.mx>
2025-06-18 11:26:47 -0600

    encriptar contras de admins de la coleccion en la BD

--- node/routes.js
+++ node/routes.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const bcrypt = require('bcrypt'); 
 const { db } = require('./firebase.config');
-const { collection, getDocs, addDoc, deleteDoc, doc, query, where } = require('firebase/firestore');
+const { collection, getDocs, addDoc, deleteDoc, updateDoc, doc, query, where } = require('firebase/firestore');
+
 const router = express.Router();
 
 // GET all
@@ -58,5 +60,32 @@ router.post('/query/:collectionName', async (req, res) => {
     res.status(500).json({ error: error.message });
   }
 });
+
+//ruta temporal para hashear las contraseñas de la coleccion de admins mediante postman
+// router.post('/hash-passwords/:collectionName', async (req, res) => {
+//   try {
+//     const colRef = collection(db, req.params.collectionName);
+//     const snapshot = await getDocs(colRef);
+
+//     const updates = [];
+
+//     for (const docSnap of snapshot.docs) {
+//       const data = docSnap.data();
+
+//       // Solo si la contraseña no está hasheada aún
+//       if (data.password && !data.password.startsWith('$2b$')) {
+//         const hashed = await bcrypt.hash(data.password, 10);
+//         const docRef = doc(db, req.params.collectionName, docSnap.id);
+//         updates.push(updateDoc(docRef, { password: hashed }));
+//       }
+//     }
+
+//     await Promise.all(updates);
+//     res.json({ message: 'Contraseñas actualizadas correctamente' });
+//   } catch (error) {
+//     console.error('Error al actualizar contraseñas:', error);
+//     res.status(500).json({ error: error.message });
+//   }
+// });
 
 module.exports = router;
