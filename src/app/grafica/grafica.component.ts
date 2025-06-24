ea56b83212d93466458f5f46f3395cff792a3318
cami-1310 <al347948@edu.uaa.mx>
2025-06-23 22:21:29 -0600

    diseño responsive de la grtafica

--- src/app/grafica/grafica.component.ts
+++ src/app/grafica/grafica.component.ts
@@ -1,86 +1,82 @@
-import { Component } from '@angular/core';
-import {NgxChartsModule,ScaleType,LegendPosition} from '@swimlane/ngx-charts';
+import { Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { NgxChartsModule, ScaleType, LegendPosition } from '@swimlane/ngx-charts';
 import { FirestoreService } from '../firestore.service';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @Component({
   selector: 'app-grafica',
+  standalone: true,
   imports: [NgxChartsModule],
   templateUrl: './grafica.component.html',
   styleUrl: './grafica.component.css'
 })
-export class GraficaComponent {
-  multi!:any[];
-  constructor(private firestoreService:FirestoreService){}
+export class GraficaComponent implements OnInit, OnDestroy {
+  multi!: any[];
+  view: [number, number] = [700, 400];
+  resizeObserver!: ResizeObserver;
+
+  @ViewChild('graficaContenedor', { static: true }) contenedorRef!: ElementRef;
+
+  constructor(private firestoreService: FirestoreService) {}
 
   ngOnInit() {
     this.firestoreService.getAll('formReservas').subscribe({
       next: (data: any[]) => {
-        console.log('Reservas obtenidas de Firestore:', data); // 
+        console.log('Reservas obtenidas de la BD:', data);
         this.multi = this.transformarDatos(data);
       },
       error: (err) => console.error(err)
     });
+
+    this.resizeObserver = new ResizeObserver(entries => {
+      for (let entry of entries) {
+        const width = entry.contentRect.width;
+        this.view = [width - 40, 400];
+      }
+    });
+
+    this.resizeObserver.observe(this.contenedorRef.nativeElement);
   }
 
-transformarDatos(datos: any[]): any[] {
-  const tiposHabitacionesDisponibles = [
-    'Cabaña Sencilla',
-    'Cabaña Doble',
-    'Cabaña Triple',
-    'Cabaña Familiar'
-  ];
-  const agrupado: { [tipo: string]: number } = {};
-  tiposHabitacionesDisponibles.forEach(tipo => {
-    agrupado[tipo] = 0;
-  });
-
-  datos.forEach(item => {
-    const tipo = item.tipoHab?.tipo;
-    const personas = item.numPersonas || 0;
-    if (tipo && agrupado.hasOwnProperty(tipo)) {
-      agrupado[tipo] += personas;
+  ngOnDestroy() {
+    if (this.resizeObserver) {
+      this.resizeObserver.disconnect();
     }
-  });
+  }
 
- 
-  const resultado = Object.keys(agrupado).map(tipo => ({
-    name: tipo,
-    value: agrupado[tipo]
-  }));
+  transformarDatos(datos: any[]): any[] {
+    const tiposHabitacionesDisponibles = [
+      'Cabaña Sencilla',
+      'Cabaña Doble',
+      'Cabaña Triple',
+      'Cabaña Familiar'
+    ];
+    const agrupado: { [tipo: string]: number } = {};
+    tiposHabitacionesDisponibles.forEach(tipo => agrupado[tipo] = 0);
 
-  return resultado;
-}
+    datos.forEach(item => {
+      const tipo = item.tipoHab?.tipo;
+      const personas = item.numPersonas || 0;
+      if (tipo && agrupado.hasOwnProperty(tipo)) {
+        agrupado[tipo] += personas;
+      }
+    });
 
+    return Object.keys(agrupado).map(tipo => ({
+      name: tipo,
+      value: agrupado[tipo]
+    }));
+  }
 
-  view: [number,number] = [700, 400];
-
-  // options
-  showXAxis: boolean = true;
-  showYAxis: boolean = true;
-  gradient: boolean = false;
-  showLegend: boolean = true;
+  // opciones del gráfico
+  showXAxis = true;
+  showYAxis = true;
+  gradient = false;
+  showLegend = true;
   legendPosition: LegendPosition = LegendPosition.Below;
-  showXAxisLabel: boolean = true;
-  yAxisLabel: string = 'Cabaña';
-  showYAxisLabel: boolean = true;
-  xAxisLabel = '#Reservaciones';
-
-  colorScheme: string = 'cool'; // o 'cool', 'natural', 'fire'
-  schemeType: ScaleType=ScaleType.Ordinal;
-
-  onSelect(data:any): void {
-    console.log('Item clicked', JSON.parse(JSON.stringify(data)));
-  }
-
-  onActivate(data:any): void {
-    console.log('Activate', JSON.parse(JSON.stringify(data)));
-  }
-
-  onDeactivate(data:any): void {
-    console.log('Deactivate', JSON.parse(JSON.stringify(data)));
-  }
+  showXAxisLabel = true;
+  xAxisLabel = '# de Reservaciones';
+  showYAxisLabel = true;
+  yAxisLabel = 'Cabaña';
+  colorScheme = 'cool';
+  schemeType = ScaleType.Ordinal;
 }
-
-
